dd3c5efb59cfc1d0712a14ded5b74cc1193664fc
Chris Simpkins <git.simpkins@gmail.com>
2015-12-19 15:12:36 -0500

    added license to script

--- src/empty_trash.sh
+++ src/empty_trash.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# empty_trash.sh
+# Empty the trash directories on OS X without the Item in Use block
+# Copyright 2015 Christopher Simpkins
+# MIT License
+
 rm -rf ~/.Trash
 rm -rf /Volumes/*/.Trashes
